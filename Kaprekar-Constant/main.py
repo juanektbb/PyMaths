c1ac4dfbb0c786ecf1d157fbbdbf7a3ac4cc15c8
Juan D. Diaz <inf.juandiaz@gmail.com>
2020-01-15 19:07:09 +0000

    Kaprekar Constant - Code finised

--- Kaprekar-Constant/main.py
+++ Kaprekar-Constant/main.py
@@ -1,10 +1,11 @@
-num = 9999
+num = 5342
 
 newNum = num
-
 numLen = len(str(num))
 
+print('Num ', 'Max ', 'Min ', 'Subs')
 
+# Limit to 8 iterations
 for x in range(0, 8):
 
 	numStr = str(newNum)
@@ -12,28 +13,31 @@ for x in range(0, 8):
 
 	subsNum = 0
 
+	# Store individual numbers in array + sort
 	numArr = []
 	for i in numStr:
 		numArr.append(str(i))
-
 	numArr.sort()
 
+	# Find largest number
 	for l in reversed(range(numLen)):
 		longNum += numArr[l]
 
+	# Find smallest number
 	for l in range(0, numLen):
 		shortNum += numArr[l]
 
+	# Substract numbers
 	subsNum = str(int(longNum) - int(shortNum))
 
+	# If new result is less than 4 digits, add zeros
 	if len(subsNum) < numLen:
-		for c in range(len(subsNum) - numLen):
-			subsNum += '0'
+		for p in range(len(subsNum), numLen):
+			subsNum = '0' + subsNum
 
-	print(newNum, longNum, shortNum, subsNum)
+	# Print and save in variable
+	print(numStr, longNum, shortNum, subsNum)
 	newNum = subsNum
 
 	if newNum == '6174':
 		break
-
-
